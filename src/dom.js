dfb6fa4e71f0a32de82fe1583a70bfac591efb0b
LeoKirasic <kirasicleo@gmail.com>
2021-11-04 22:37:52 +0100

    Add rough page layout

---
+++ src/dom.js
@@ -0,0 +1,63 @@
+function createDom(data) {
+  const container = document.getElementById('container');
+
+  const header = document.createElement('div');
+  header.id = 'header';
+  const headerIcon = document.createElement('div');
+  headerIcon.id = 'header-icon';
+  headerIcon.textContent = 'X';
+  const headerText = document.createElement('div');
+  headerText.id = 'header-text';
+  headerText.textContent = 'Weather';
+  header.appendChild(headerIcon);
+  header.appendChild(headerText);
+  container.appendChild(header);
+
+  const form = document.createElement('form');
+  form.id = 'form';
+  const searchInput = document.createElement('input');
+  searchInput.id = 'search-input';
+  const searchButton = document.createElement('button');
+  searchButton.id = 'search-button';
+  searchButton.setAttribute('type', 'button');
+  form.appendChild(searchInput);
+  form.appendChild(searchButton);
+  container.appendChild(form);
+
+  const currentWeatherContainer = document.createElement('div');
+  currentWeatherContainer.id = 'current-weather-container';
+  const city = document.createElement('div');
+  city.id = 'city';
+  city.textContent = 'Ogulin';
+  const currentTemperature = document.createElement('div');
+  currentTemperature.id = 'current-temperature';
+  currentTemperature.textContent = '8';
+  currentWeatherContainer.appendChild(city);
+  currentWeatherContainer.appendChild(currentTemperature);
+
+  container.appendChild(currentWeatherContainer);
+
+  const hourlyWeatherContainer = document.createElement('ul');
+  hourlyWeatherContainer.id = 'hourly-weather-container';
+
+  for (let i = 0; i <= 24; i++) {
+    const hourlyWeatherCard = document.createElement('li');
+    hourlyWeatherCard.classList = 'hourly-weather-card';
+    hourlyWeatherCard.textContent = i;
+    hourlyWeatherContainer.appendChild(hourlyWeatherCard);
+  }
+  container.appendChild(hourlyWeatherContainer);
+
+  const dailyWeatherContainer = document.createElement('ul');
+  dailyWeatherContainer.id = 'daily-weather-container';
+
+  for (let i = 0; i <= 7; i++) {
+    const dailyWeatherCard = document.createElement('li');
+    dailyWeatherCard.classList = 'daily-weather-card';
+    dailyWeatherCard.textContent = i;
+    dailyWeatherContainer.appendChild(dailyWeatherCard);
+  }
+  container.appendChild(dailyWeatherContainer);
+}
+
+export { createDom };
